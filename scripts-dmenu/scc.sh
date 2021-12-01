2f68ce69e54d9f101f8903d3d6b7aff83b36dc3d
André Gonzalez <lopescg@gmail.com>
2021-11-30 21:26:47 -0300

    corrected flags

--- scripts-dmenu/scc.sh
+++ scripts-dmenu/scc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #Copy it to /usr/local/bin in order to execute iit with dmenu
-scrot -sfe 'xclip -selection clipboard -t image/png -i $f'
+scrot -s -f -e 'xclip -selection clipboard -t image/png -i $f'
 
