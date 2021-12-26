64040682dd050a4ea56725b63b7ebdcc491d600e
André Gonzalez <lopescg@gmail.com>
2021-12-26 09:18:28 -0300

    changed ocurrences of sudo to doas

--- packages-list/debian/dbeaver-install.sh
+++ packages-list/debian/dbeaver-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
-echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
-sudo apt update
-sudo apt -y install dbeaver-ce
+wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | doas apt-key add -
+echo "deb https://dbeaver.io/debs/dbeaver-ce /" | doas tee /etc/apt/sources.list.d/dbeaver.list
+doas apt update
+doas apt -y install dbeaver-ce
 
