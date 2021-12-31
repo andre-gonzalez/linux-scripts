70ddd1b0730a1de7f3f37be1b1bf8d0b96c49738
André Gonzalez <lopescg@gmail.com>
2021-12-31 09:49:34 -0300

    added the option to use dvorak not international

--- scripts-dmenu/dvorak.sh
+++ scripts-dmenu/dvorak.sh
@@ -1,3 +1,4 @@
-setxkbmap -layout us,us -variant ,dvorak-intl -option "lv3:rwin_switch,grp:alt_space_toggle"
+setxkbmap -layout us,us -variant ,dvorak -option "lv3:rwin_switch,grp:alt_space_toggle"
+# setxkbmap -layout us,us -variant ,dvorak-intl -option "lv3:rwin_switch,grp:alt_space_toggle"
 # change esc with caps lock
 setxkbmap -option caps:escape
