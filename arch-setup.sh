60c99f4d94cfc8c220f2510760dc70106b2c77e8
André Gonzalez <lopescg@gmail.com>
2022-02-04 20:41:24 -0300

    removed a to-do

--- arch-setup.sh
+++ arch-setup.sh
@@ -108,4 +108,3 @@ usermod --append --groups libvirt `whoami`
 # 2. Configure a way to use dmenu for opening directories using fzf
 # 3. Configure notification system for when the computer battery is getting to low
 # 4. Configure keyboard shortcuts in st terminal
-# 5. Configure firewall
