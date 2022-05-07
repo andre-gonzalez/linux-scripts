cfd6adf9703b919e3e4bdce3d8e6cf4105d2f15c
André Gonzalez <lopescg@gmail.com>
2022-05-07 18:13:17 -0300

    convert this repository to a repository to store linux scripts

--- packages-list/debian/dbeaver-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | doas apt-key add -
-echo "deb https://dbeaver.io/debs/dbeaver-ce /" | doas tee /etc/apt/sources.list.d/dbeaver.list
-doas apt update
-doas apt -y install dbeaver-ce
-
--- packages-list/debian/insync-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-doas apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
-deb http://apt.insync.io/debian bullseye non-free contrib
-doas apt-get update
-doas apt-get install insync
