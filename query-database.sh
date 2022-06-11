d1547cbffca1a640028f5ae185e05971852131e5
André Gonzalez <lopescg@gmail.com>
2022-06-11 19:20:54 -0300

    changed from sed to head and tail for speed improvment

--- query-database.sh
+++ query-database.sh
@@ -2,9 +2,9 @@
 
 access_file=
 type=$(head -n 1 $access_file)
-connection=$(sed -n 2p $access_file)
-database=$(sed -n 3p $access_file)
-user=$(sed -n 4p $access_file)
+connection=$(head -n 2 $access_file | tail -n 1)
+database=$(head -n 3 $access_file | tail -n 1)
+user=$(head -n 4 $access_file | tail -n 1)
 pass=$(tail -n 1 $access_file)
 
 sql2csv --db $type://$user:$pass@$connection/$database --query "$1"
