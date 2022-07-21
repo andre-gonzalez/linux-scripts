3b7c842042682b70d203e1f04291ed0605918b6a
André Gonzalez <lopescg@gmail.com>
2022-07-21 12:27:50 -0300

    added a script to insert into databases

---
+++ insert-database.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#########################################################################
+#                               PREREQUISITES                           #
+#########################################################################
+# 1. csvkit
+
+#########################################################################
+#                               INSTRUCTIONS                            #
+#########################################################################
+# - Create a file with the database access data with this line structure:
+#   1. Type of databse (postgresql, mysql, sqllite...)
+#   2. Connection parameters (Ex: localhost@3308)
+#   3. Database name (Ex: mysqldatabase)
+#   4. User name (Ex: myuser)
+#   5. Password ( Ex: mypassword)
+
+access_file=$1
+type=$(head -n 1 $access_file)
+connection=$(head -n 2 $access_file | tail -n 1)
+database=$(head -n 3 $access_file | tail -n 1)
+user=$(head -n 4 $access_file | tail -n 1)
+pass=$(tail -n 1 $access_file)
+
+csvsql --db $type://$user:$pass@$connection/$database --insert "$2"
