0c2a0015dde6fcd9a49b6cd1276ac966961eb2a4
André Gonzalez <lopescg@gmail.com>
2022-08-13 19:21:07 -0500

    added a script to backup fish_history using ansible

---
+++ backup-fish-history.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cp -f "$HOME"/.local/share/fish/fish_history "$HOME"/gdrive-pessoal/pessoal/programação/ansible/main-ansible/files/
+cd "$HOME"/gdrive-pessoal/pessoal/programação/ansible/main-ansible/ && ansible-vault encrypt ./files/fish_history
+git add ./files/fish_history
+git commit -m "update fish_history file $(date "+%Y-%m-%d-%R")"
+git push -u origin main
