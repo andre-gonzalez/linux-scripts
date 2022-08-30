709730199b3f1a89aac0d667bdf3086ec5d5e689
André Gonzalez <lopescg@gmail.com>
2022-08-29 19:52:08 -0500

    fix comparison sign

--- backup-to-internal-hard-drive.sh
+++ backup-to-internal-hard-drive.sh
@@ -5,7 +5,7 @@ USED_SPACE_HOME=`df /home | awk '{print $3}' | tail -n 1`
 WILL_FIT=$(expr $FREE_SPACE - $USED_SPACE_HOME)
 
 # exclude the oldest backup until the drive can fit the home directory
-while [ $WILL_FIT > 0 ];
+while [ $WILL_FIT < 0 ];
 do
 		FILE_TO_EXCLUDE=`ls -1 /mnt/hd-interno/ | head -n 1`
 		rm -fr /mnt/hd-interno/$FILE_TO_EXCLUDE
