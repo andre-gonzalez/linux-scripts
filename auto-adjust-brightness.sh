f3da2f2fb7d2487146e36b3569622ac92565308c
André Gonzalez <lopescg@gmail.com>
2022-11-21 13:59:16 -0600

    invert if conditions

--- auto-adjust-brightness.sh
+++ auto-adjust-brightness.sh
@@ -4,41 +4,41 @@ adjust_bright () {
 		hour=$(date +%H)
 		minute=$(date +%M)
 
-		if [ "$hour" -eq 17 -a "$minute" -eq 30 ]; then
-				brightnessctl set 97%; kill -45 $(pidof dwmblocks)
+		if [ "$hour" -eq 18 -a "$minute" -ge 30 ]; then
+				brightnessctl set 50%; kill -45 $(pidof dwmblocks)
 				exit
-		elif [ "$hour" -eq 17 -a "$minute" -eq 35 ]; then
-				brightnessctl set 92%; kill -45 $(pidof dwmblocks)
-				exit
-		elif [ "$hour" -eq 17 -a "$minute" -eq 40 ]; then
-				brightnessctl set 89%; kill -45 $(pidof dwmblocks)
-				exit
-		elif [ "$hour" -eq 17 -a "$minute" -eq 45 ]; then
-				brightnessctl set 84%; kill -45 $(pidof dwmblocks)
-				exit
-		elif [ "$hour" -eq 17 -a "$minute" -eq 50 ]; then
-				brightnessctl set 79%; kill -45 $(pidof dwmblocks)
-				exit
-		elif [ "$hour" -eq 17 -a "$minute" -eq 55 ]; then
-				brightnessctl set 74%; kill -45 $(pidof dwmblocks)
-				exit
-		elif [ "$hour" -eq 18 -a "$minute" -eq 00 ]; then
-				brightnessctl set 69%; kill -45 $(pidof dwmblocks)
-				exit
-		elif [ "$hour" -eq 18 -a "$minute" -eq 05 ]; then
-				brightnessctl set 65%; kill -45 $(pidof dwmblocks)
-				exit
-		elif [ "$hour" -eq 18 -a "$minute" -eq 10 ]; then
-				brightnessctl set 61%; kill -45 $(pidof dwmblocks)
-				exit
-		elif [ "$hour" -eq 18 -a "$minute" -eq 15 ]; then
-				brightnessctl set 57%; kill -45 $(pidof dwmblocks)
-				exit
-		elif [ "$hour" -eq 18 -a "$minute" -eq 20 ]; then
+		elif [ "$hour" -eq 18 -a "$minute" -ge 20 ]; then
 				brightnessctl set 53%; kill -45 $(pidof dwmblocks)
 				exit
-		elif [ "$hour" -eq 18 -a "$minute" -eq 30 ]; then
-				brightnessctl set 50%; kill -45 $(pidof dwmblocks)
+		elif [ "$hour" -eq 18 -a "$minute" -ge 15 ]; then
+				brightnessctl set 57%; kill -45 $(pidof dwmblocks)
+				exit
+		elif [ "$hour" -eq 18 -a "$minute" -ge 10 ]; then
+				brightnessctl set 61%; kill -45 $(pidof dwmblocks)
+				exit
+		elif [ "$hour" -eq 18 -a "$minute" -ge 05 ]; then
+				brightnessctl set 65%; kill -45 $(pidof dwmblocks)
+				exit
+		elif [ "$hour" -eq 18 -a "$minute" -ge 00 ]; then
+				brightnessctl set 69%; kill -45 $(pidof dwmblocks)
+				exit
+		elif [ "$hour" -eq 17 -a "$minute" -ge 55 ]; then
+				brightnessctl set 74%; kill -45 $(pidof dwmblocks)
+				exit
+		elif [ "$hour" -eq 17 -a "$minute" -ge 50 ]; then
+				brightnessctl set 79%; kill -45 $(pidof dwmblocks)
+				exit
+		elif [ "$hour" -eq 17 -a "$minute" -ge 45 ]; then
+				brightnessctl set 84%; kill -45 $(pidof dwmblocks)
+				exit
+		elif [ "$hour" -eq 17 -a "$minute" -ge 40 ]; then
+				brightnessctl set 89%; kill -45 $(pidof dwmblocks)
+				exit
+		elif [ "$hour" -eq 17 -a "$minute" -ge 35 ]; then
+				brightnessctl set 92%; kill -45 $(pidof dwmblocks)
+				exit
+		elif [ "$hour" -eq 17 -a "$minute" -ge 30 ]; then
+				brightnessctl set 97%; kill -45 $(pidof dwmblocks)
 				exit
 		fi
 }
