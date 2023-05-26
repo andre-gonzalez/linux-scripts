dc366d6a2f4d6d2cc4eb50a089828a2568044483
André Gonzalez <lopescg@gmail.com>
2023-05-25 19:09:45 -0600

    healthcheck to confirm job is running

--- backup-fish-history.sh
+++ backup-fish-history.sh
@@ -5,3 +5,5 @@ cd "$HOME"/gdrive-pessoal/pessoal/programação/ansible/main-ansible/ && ansible
 git add ./files/fish_history
 git commit -m "update fish_history file $(date "+%Y-%m-%d-%R")"
 git push -q -u origin main
+
+curl https://hc-ping.com/9620c142-9e54-465d-9d82-35c5231349f2
--- push-new-notes.sh
+++ push-new-notes.sh
@@ -2,3 +2,5 @@
 
 NOW=$(date +%Y-%m-%d_%T)
 cd "$HOME"/gdrive-pessoal/pessoal/obsidian && git add -A && git commit -am "$NOW" && git push -q -u origin main
+
+curl https://hc-ping.com/fde92570-863b-4c76-a1c1-567c6f321a00
