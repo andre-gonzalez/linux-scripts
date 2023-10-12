b7a58db3e4bb5ce7e24e47238ea6c93ca5394b0b
André Gonzalez <lopescg@gmail.com>
2023-10-12 16:25:11 -0600

    made git pull pull all branches

--- pull-repositories.sh
+++ pull-repositories.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-cd "$HOME"/eureciclo/airflow/main-airflow/ && git pull
-cd "$HOME"/eureciclo/datalake-infra/main-datalake-infra/ && git pull
-cd "$HOME"/eureciclo/datalake-infra/main-datalake-infra/ && git pull
-cd "$HOME"/eureciclo/databricks-notebooks/main-databricks-notebooks/ && git pull
-cd "$HOME"/eureciclo/databricks-notebooks/main-databricks-notebooks/ && git pull
-cd "$HOME"/eureciclo/salesops-dw/main-salesops-dw-DO-NOT-TOUCH/ && git pull
-cd "$HOME"/eureciclo/utils/main-utils-DO-NOT-TOUCH/ && git pull
+cd "$HOME"/eureciclo/airflow/main-airflow/ && git pull --all
+cd "$HOME"/eureciclo/datalake-infra/main-datalake-infra/ && git pull --all
+cd "$HOME"/eureciclo/datalake-infra/main-datalake-infra/ && git pull --all
+cd "$HOME"/eureciclo/databricks-notebooks/main-databricks-notebooks/ && git pull --all
+cd "$HOME"/eureciclo/databricks-notebooks/main-databricks-notebooks/ && git pull --all
+cd "$HOME"/eureciclo/salesops-dw/main-salesops-dw-DO-NOT-TOUCH/ && git pull --all
+cd "$HOME"/eureciclo/utils/main-utils-DO-NOT-TOUCH/ && git pull --all
