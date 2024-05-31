a4978f94b51f2135d8917ea9a751d6c50c749636
André Gonzalez <lopescg@gmail.com>
2024-05-31 07:10:53 -0600

    update path to notes directory

--- copy-to-clipboard.sh
+++ copy-to-clipboard.sh
@@ -3,7 +3,7 @@
 # This script pipes a file into dmenu and put the text in the clipboard based
 # on the selection made
 
-LIST="$HOME/gdrive-pessoal/pessoal/obsidian/pessoal/copy_to_clipboard.md"
+LIST="$HOME/projects/notas/pessoal/copy_to_clipboard.md"
 
 SELECTION=$(dmenu -p "Copy:" -l 50 < $LIST)
 
--- push-new-notes.sh
+++ push-new-notes.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 NOW=$(date +%Y-%m-%d_%T)
-cd "$HOME"/gdrive-pessoal/pessoal/obsidian && git add -A && git commit -am "$NOW" && git push -q -u origin main
+cd "$HOME"/projects/notas/ && git add -A && git commit -am "$NOW" && git push -q -u origin main
 
 curl https://hc-ping.com/fde92570-863b-4c76-a1c1-567c6f321a00
