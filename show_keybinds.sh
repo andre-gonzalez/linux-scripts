a5648c4cdd6dadabc01904cc8a7c1842366359ed
André Gonzalez <lopescg@gmail.com>
2024-08-14 18:23:29 -0600

    Fix/ file location and position on ultra wide screen of script show_keybinds

--- show_keybinds.sh
+++ show_keybinds.sh
@@ -1,1 +1,1 @@
-stw -A 0.6 -x 43% -y 30% -a l -p 0 -F 'Liberation Mono Bold:size10' cat ~/gdrive-pessoal/pessoal/obsidian/pessoal/keybinds-to-show-on-screen.md
+stw -A 0.6 -x 0% -y 20% -a l -p 0 -F 'Liberation Mono Bold:size10' cat ~/projects/notas/pessoal/keybinds-to-show-on-screen.md
