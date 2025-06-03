e9e35ee79385c96bfed57f6fad04d8be3705e987
André Gonzalez <lopescg@gmail.com>
2025-06-02 19:03:58 -0600

    Copy to Clipboard/ change script to interpret || as new line

--- copy-to-clipboard.sh
+++ copy-to-clipboard.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 # This script pipes a file into dmenu and put the text in the clipboard based
-# on the selection made
+# on the selection madeLIST="$HOME/projects/notas/pessoal/copy_to_clipboard.md"
 
 LIST="$HOME/projects/notas/pessoal/copy_to_clipboard.md"
 
-SELECTION=$(dmenu -p "Copy:" -l 50 < $LIST)
+SELECTION=$(dmenu -p "Copy:" -l 50 <"$LIST")
 
-echo $SELECTION | awk -F '|' '{print $2}' | xargs | xclip -r -selection clipboard
+TEXT=$(echo "$SELECTION" | sed 's/^[^|]*|//; s/^[ \t]*//; s/[ \t]*$//')
+
+PROCESSED=$(echo "$TEXT" | sed 's/||/\
+/g' | sed 's/^[ \t]*//')
+
+echo "$PROCESSED" | xclip -r -selection clipboard
