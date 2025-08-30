78f176b28cf6930ecd07a2efdff8dd0e520d696b
André Gonzalez <lopescg@gmail.com>
2025-08-30 17:07:17 -0300

    Set only-big-screen to use 103 DPI,rate to 100 hz and scale to 1x1

--- only-big-screen.sh
+++ only-big-screen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output HDMI-1 --primary --mode 2560x1080 --pos 1920x0 --rotate normal
+xrandr --output eDP-1 --off --output HDMI-1 --primary --mode 2560x1080 --rate 100 --dpi 103 --scale 1x1
