c949a64d7c50f08d0c9190fb2e77e0f3af6c9f12
André Gonzalez <lopescg@gmail.com>
2025-10-14 17:29:54 -0300

    add script to change dwm between laptop and ultra wide monitor modes

--- only-big-screen.sh
+++ only-big-screen.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output HDMI-1 --primary --mode 2560x1080 --rate 100 --dpi 103 --scale 1x1
+xrandr --output eDP-1 --off --output HDMI-1 --primary --mode 2560x1080 --rate 100 --dpi 103 --scale 1x1 ;
+cdwm ultra
