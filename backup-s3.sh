2b883d5a9671a339074f891b6d4f0270ae997ad9
André Gonzalez <lopescg@gmail.com>
2025-11-02 16:20:26 -0300

    Add script to backup files to S3 using rclone

---
+++ backup-s3.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Rclone script to archive Jeff Geerling's most important data to an Amazon S3
+# Glacier Deep Archive-backed bucket.
+#
+# Basic usage:
+#   ./rclone.sh
+#
+# Script requires valid credentials - set up with `rclone config`.
+
+RCLONE=/usr/bin/rclone
+
+# Check if rclone is installed.
+if ! [ -x "$(command -v $RCLONE)" ]; then
+  echo 'Error: rclone is not installed.' >&2
+  exit 1
+fi
+
+# Don't run if an instance of rclone is already running.
+if ps -ef | grep -v grep | grep rclone ; then
+  exit 0
+fi
+
+# Variables.
+rclone_remote=personal
+rclone_s3_bucket=backups-pessoal
+bandwidth_limit=100M
+
+# Make sure bucket exists.
+$RCLONE mkdir $rclone_remote:$rclone_s3_bucket
+
+# List of directories to clone. MUST be absolute path, beginning with /.
+declare -a dirs=(
+  "/mnt/hd-externo/s3/arch-samsung/"
+  "/mnt/hd-externo/s3/server/"
+  "/mnt/hd-externo/s3/arch-samsung-wifi-networks/"
+)
+
+# Copy files that do not exist in the bucket
+for i in "${dirs[@]}"
+do
+	echo "Syncing Directory: $i"
+	despaced="${i// /_}"
+	$RCLONE copy "$i" $rclone_remote:$rclone_s3_bucket"$despaced" --skip-links --ignore-existing # --bwlimit $bandwidth_limit
+done
