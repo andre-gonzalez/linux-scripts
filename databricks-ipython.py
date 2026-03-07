1cceec55a61d4349c049742b206736881e01df03
André Gonzalez <lopescg@gmail.com>
2026-03-07 19:25:22 -0300

    Remove old not used anymore scripts

--- databricks-ipython.py
+++
@@ -1,6 +0,0 @@
-from databricks.connect import DatabricksSession
-from databricks.sdk.core import Config
-from IPython.display import display
-config = Config(profile = "DEFAULT")
-spark = DatabricksSession.builder.sdkConfig(config).getOrCreate()
-dbutils = DBUtils(spark)
